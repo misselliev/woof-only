4589cb62425a30dc0ca2407911756ab08e6ccb3c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-27 17:40:25 -0700

    Setting up remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :assign_token
+
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def assign_token
+    self.remember_token = User.new_token
+    self.remember_digest = Digest::SHA1.hexdigest(remember_token)
+  end
+
+  def remember
+    self.remember_token = new_token
+    update_attribute(:remember_digest, Digest::SHA1.hexdigest(remember_token))
+  end
 end
