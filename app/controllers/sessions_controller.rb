fca6eef1dd7bb2db705fd86f2859428e6c15f1e6
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-28 19:17:43 -0700

    login/logout added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email]) 
+    if user && user.authenticate(params[:password_digest])
+      flash[:sucess]="You have logged in!"
+      log_in user
+      redirect_to @user
+    else
+      flash[:danger]="Oops, please check your input"
+      render 'new' 
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    flash[:sucess]="You have logged out!"
+    redirect_to root_url
   end
 
 end
